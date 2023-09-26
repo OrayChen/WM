c7cadfd88ef93634ad52dacf896ca127217adc79
OrayChen <2837515834@qq.com>
2023-09-26 18:19:00 +0800

    202309261820

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font Mono:style=SemiBold:pixelsize=24:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:style=SemiBold:pixelsize=24";
 static int borderpx = 2;
 
 /*
