cbe9ec0a961a9e741e93fcf0d62004e7463c2fc2
OrayChen <2837515834@qq.com>
2023-09-26 18:25:10 +0800

    202309261825

--- .dwm/scripts/trayer.sh
+++ .dwm/scripts/trayer.sh
@@ -1,5 +1,5 @@
 if [ "$(ps ax|grep -v grep|grep -v scripts|grep trayer)" == "" ] ; then
-	eval "trayer --align center --width 30 --SetDockType false --transparent true --tint 0x88888888 --expand false &"
+	eval "trayer --align center --width 20 --height 5 --SetDockType false --transparent true --tint 0x88888888 --expand false &"
 else
 	eval "killall trayer"
 fi
